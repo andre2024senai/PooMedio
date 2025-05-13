27c75ab84660d89407a6cb00de2036a06eda688b
andre2024senai <154605066+andre2024senai@users.noreply.github.com>
2025-05-13 17:40:28 -0300

    Exercicio 1

---
+++ src/Faca.java
@@ -0,0 +1,9 @@
+public class Faca {
+    String nome;
+    String material;
+
+    public void atacar(){
+        System.out.println("Atacando!!!");
+    }
+
+}
---
+++ src/FacaMain.java
@@ -0,0 +1,7 @@
+public class FacaMain {
+    public static void main(String[] args) {
+
+        Faca cutelo = new Faca();
+        cutelo.atacar();
+    }
+}
