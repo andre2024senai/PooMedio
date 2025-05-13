b73c681d49e03244d8722aabc10dd46e0f19cc6b
andre2024senai <154605066+andre2024senai@users.noreply.github.com>
2025-05-13 17:52:24 -0300

    Ex2

---
+++ src/Personagem.java
@@ -0,0 +1,10 @@
+public class Personagem {
+    String nome;
+    String classe;
+
+    public void subirNivel(){
+        System.out.println("Subir Nivel");
+
+    }
+
+}
---
+++ src/PersonagemMain.java
@@ -0,0 +1,9 @@
+public class PersonagemMain {
+    public static void main(String[] args) {
+        Personagem person = new Personagem();
+
+        person.nome = "Joao";
+        person.subirNivel();
+
+    }
+}
